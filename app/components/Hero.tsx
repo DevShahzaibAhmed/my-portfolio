45749daa93303ae91ebb9e7fc88d63fea9bdab12
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-31 04:22:08 +0500

    updated

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
+
 const Hero = () => {
   return (
-    <div className='bg-[url("/b2.jpg")] h-screen bg-cover bg-center bg-fixed flex items-center'>
+    <div className='bg-[url("/b3.jpg")] h-screen bg-cover bg-center bg-fixed flex items-center'>
 
       <div className='cotainer mx-auto px-4'>
 
         <div className='max-w-[450px] text-white flex flex-col gap-[40px]'>
+
+          <div>
+            <h1 className='text-5xl '>I`m SHAHZAIB AHMED</h1>
+
+          </div>
 
         </div>
 
