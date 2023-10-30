1c63ea6b9eda126f6908993358b45c93edd6675b
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-10-31 00:53:44 +0500

    updated

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,24 +1,15 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='bg-[url("/hero.jpg")] bg-cover h-screen opacity-75 object-right-top'>
-
-      <div className='cotainer mx-auto px-4'>
-
-        <div className='max-w-[450px] text-white flex flex-col gap-[40px]'>
-
-          <div>
-
-          </div>
-
-        </div>
-
+    <div className='h-screen flex flex-col opacity-75 object-right-top '>
+      <div className="flex w-full justify-end items-center">
+        <Image className="w-[10rem] h-[10rem]" src='/bg-1.jpg' width={1000} height={1000} alt="Profile Pic"/>
       </div>
-
-
     </div>
-  )
-}
 
-export default Hero
+  );
+};
+
+export default Hero;
