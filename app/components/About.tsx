f7097641aaeb453f4199d7e505939c5ac3b2a8b8
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-11-01 00:52:30 +0500

    updated

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import {AiOutlineGift} from 'react-icons/ai'
+import { FiBookOpen } from 'react-icons/fi'
+import { BsGlobe2 } from 'react-icons/bs'
+import { TbPacman } from 'react-icons/tb'
+import { IoLocationOutline } from 'react-icons/io5'
+
 
 const About = () => {
   return (
@@ -16,7 +22,7 @@ const About = () => {
             <Image
             src="/profile.jpg"
              width={400}
-            height={500}
+            height={10}
              alt="Profile-Pic"
             />
 
@@ -24,12 +30,58 @@ const About = () => {
         <div className='flex items-center justify-between w-[300px]'>
             <div className='flex flex-col gap-6'>
                 <div className='flex gap-4 items-center font-medium'>
-                    
+                <AiOutlineGift className='text-[#ff4D41] text-[22px]'/>
+                BirthDay
+                </div>
 
+                <div className='flex gap-4 items-center font-medium'>
+                <FiBookOpen className='text-[#ff4D41] text-[22px]'/>
+                Education
+                </div>
+
+                <div className='flex gap-4 items-center font-medium'>
+                <BsGlobe2 className='text-[#ff4D41] text-[22px]'/>
+                Website
+                </div>
+
+                <div className='flex gap-4 items-center font-medium'>
+                <TbPacman className='text-[#ff4D41] text-[22px]'/>
+                Interest
+                </div>
+
+                <div className='flex gap-4 items-center font-medium'>
+                <IoLocationOutline className='text-[#ff4D41] text-[22px]'/>
+                Location
                 </div>
 
             </div>
+            <div className='flex flex-col gap-6 text-gray-600'>
+                <div>:</div>
+                <div>:</div>
+                <div>:</div>
+                <div>:</div>
+                <div>:</div>
+            </div>
 
+            <div className='flex flex-col gap-6 text-gray-600'>
+            <div>04/09/2000</div>            
+            <div>University Of Central</div>
+            <div><a href="https://github.com/DevShahzaibAhmed"></a></div>
+            <div>Front End Development</div>
+            <div>Lahore</div>          
+
+            </div>
+
+        </div>
+
+        <div className='max-w-[800px]'>
+            <h2 className='font-bold mt-16 mb-10 '>I am Shahzaib. A FrontEnd Developer</h2>
+            <p className='text-gray-600'>Hi! My Name is Shahzaib Ahmed. I am a FrontEnd Developer with a good knowledge and experience.</p>
+
+            <button className='bg-[#ff4D41] text-white py-2 px-4 rounded inline-block mt-10 font-medium border border-[#ff4D41] hover:bg-transparent hover:text-[#ff4D41] transition duration-200'>
+              Contact ME
+            </button>
+            
         </div>
 
     </div>
