2b82cf07588c8ceddb08323ef412e14206f40d09
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-11-01 05:09:08 +0500

    updated

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -11,6 +11,53 @@ const Skills = () => {
     </div>
 
     <p className='text-gray-600 -mt-10 mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, ipsa.</p>
+   
+   <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+
+            <div>
+                <p>React Js</p>
+                <div className='relative bg-gray-400 h-[4px] mt-2 '>
+                    <div className='bg-[#ff4D41] absolute h-[4px] w-[90%]'></div>
+                </div>
+            </div>
+
+            <div>
+                <p>Next Js</p>
+                <div className='relative bg-gray-400 h-[4px] mt-2 '>
+                    <div className='bg-[#ff4D41] absolute h-[4px] w-[85%]'></div>
+                </div>
+            </div>
+
+            <div>
+                <p>Taiwind CSS</p>
+                <div className='relative bg-gray-400 h-[4px] mt-2 '>
+                    <div className='bg-[#ff4D41] absolute h-[4px] w-[95%]'></div>
+                </div>
+            </div>
+            
+            <div>
+                <p>PHP</p>
+                <div className='relative bg-gray-400 h-[4px] mt-2 '>
+                    <div className='bg-[#ff4D41] absolute h-[4px] w-[80%]'></div>
+                </div>
+            </div>
+            
+            <div>
+                <p>Laravel</p>
+                <div className='relative bg-gray-400 h-[4px] mt-2 '>
+                    <div className='bg-[#ff4D41] absolute h-[4px] w-[75%]'></div>
+                </div>
+            </div>
+
+            <div>
+                <p>Node Js</p>
+                <div className='relative bg-gray-400 h-[4px] mt-2 '>
+                    <div className='bg-[#ff4D41] absolute h-[4px] w-[70%]'></div>
+                </div>
+            </div>
+
+   </div>
+   
     </section>
     </>
   )
