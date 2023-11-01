c164c85a7e69dc99c19c4a13c35cd6b0362b98a4
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-11-01 06:30:12 +0500

    updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-4xl'>
+    <div className="bg-cover bg-[url('/footer.jpg')] bg-center h-[200px] text-white grid place-items-center">
       
+      <div className='text-center'>
+        <p>Developed in Next Js With Tailwind CSS</p>
+        <p>&copy; 2023 CopyRight Reserved</p>
+
+      </div>
     </div>
   )
 }
