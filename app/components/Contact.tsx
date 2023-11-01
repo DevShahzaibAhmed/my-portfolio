f3f6b2ba52a2db57a43d2c3ad3ddf4f5d1a082b1
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-11-01 06:10:52 +0500

    updated

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -13,7 +13,18 @@ const Contact = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
 
                 <input type="text" placeholder='Name' className='inputStyle' required/>
+                <input type="text" placeholder='Email' className='inputStyle' required/>
+
             </div>
+
+            <input type="text" placeholder='Subject' className='inputStyle' required/>
+
+            <textarea rows={6} placeholder='Message' className='inputStyle' required ></textarea>
+
+            <div>
+                <button type="submit" className='btn'>MESSAGE</button>
+            </div>
+
 
         </form>
     </section>
