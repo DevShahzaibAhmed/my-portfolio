d9c06479309b89eb7b94d701dfa014fb4b0edd25
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-11-01 06:13:42 +0500

    updated

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -75,16 +75,6 @@ const About = () => {
 
         </div>
 
-        <div className=' max-w-[800px] '>
-            <h2 className='font-bold mt-16 mb-10'>I am Shahzaib. A FrontEnd Developer</h2>
-            <p className='text-gray-600'>Hi! My Name is Shahzaib Ahmed. I am a FrontEnd Developer with a good knowledge and experience.</p>
-
-            <button className='bg-[#ff4D41] text-white py-2 px-4 rounded inline-block mt-10 font-medium border border-[#ff4D41] hover:bg-transparent hover:text-[#ff4D41] transition duration-200'>
-              Contact ME
-            </button>
-        
-        </div>
-
     </div>
     
 
