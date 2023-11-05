902743a0a81054a2864e7a69660e4d13f2034c81
devShahzaibAhmed <shahzaib.ahmed4362@gmail.com>
2023-11-05 23:05:20 +0500

    updated

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,21 +12,18 @@ const Hero = () => {
           <div >
             <h1 className='text-5xl hover:underline transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 '>Shahzaib Ahmed</h1>
             <h4 className='text-2xl mt-3 '>FrontEnd Developer</h4>
-
           </div>
 
           <div className='bg-[#ff4D41] h-[2px] w-[40px]'></div>
 
           <div>
             <p>Welcome to My Portfolio Website</p>
-
             <button className='btn'>
               Learn More
             </button>
           </div>
 
         </div>
-
       </div>
     </div>
   )
